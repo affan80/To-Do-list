e25d8bb61bf61c535a459a09a61352e1a415ad08
affan80 <mrzaidzaid202@gmail.com>
2025-04-03 13:12:13 +0530

    Update 2

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
- const task = document.getElementById(task)
- const listcontainer = document.getElementById(list-container)
-//  const list = document.getElementById(list)
+const task = document.getElementById("task");
+const listcontainer = document.getElementById("list");
+// const list = document.getElementById("list");
 
-function addTask(){
-    if(task.value === ''){
-        alert("You must write somthing!") 
-    }
-    else{
-        let li = document.createElement('li')
+function addTask() {
+    if (task.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement('li');
         li.innerHTML = task.value;
-        listcontainer.appendChild(li)
-         
+        listcontainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = 'X';
+        li.appendChild(span);
     }
+    task.value = '';
+    saveData();
 }
+
+listcontainer.addEventListener("click", function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem('data', listcontainer.innerHTML);
+}
+
+function showTask() {
+    listcontainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
